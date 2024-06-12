04e4afaa3940f50cc4040050859af6357dacd578
AlienegraGeek <969024145@qq.com>
2024-06-12 17:29:37 +0800

    File transfer component

--- src/components/FileTransfer.vue
+++ src/components/FileTransfer.vue
@@ -1,36 +1,16 @@
-<template>
-  <h2 class="left-align">MinIO上传文件</h2>
-  <div class="row">
-    <div>
-      <input id="inputFile" type="file" @change="handleFileChange" />
-      <button @click="uploadFile">上传</button>
-    </div>
-    <div>
-      <input type="text" v-model="fileName" placeholder="输入文件名" />
-      <button @click="downloadFile">下载</button>
-    </div>
-    <div>
-      <div>组件</div>
-      <FileTransfer bucket="my-bucket" objectName="example" mode="upload" />
-      <FileTransfer bucket="my-bucket" objectName="example.png" mode="download" />
-    </div>
-  </div>
-</template>
-
-<script lang="ts" setup>
-import {ref} from 'vue'
+<script setup lang="ts">
+import {ref, defineProps} from 'vue'
 import axios from 'axios'
-import FileTransfer from "@/components/FileTransfer.vue";
-
-const selectedFile = ref<File | null>(null)
-const fileName = ref<string>('')
 
 interface Props {
   bucket: string
-  object_name: string
+  objectName: string
+  mode: 'upload' | 'download'
 }
 
 const props = defineProps<Props>()
+
+const selectedFile = ref<File | null>(null)
 
 const handleFileChange = (event: Event) => {
   const target = event.target as HTMLInputElement
@@ -44,15 +24,12 @@ const uploadFile = async () => {
     alert('Please select a file first.')
     return
   }
-  
-  const file = selectedFile.value
-  const objectName = file.name
-  
+  const objectName = replaceFileName(selectedFile.value.name)
   try {
     // 获取预签名 URL
-    const response = await axios.get('http://127.0.0.1:2040/api/uploadPre', {
+    const response = await axios.get('http://localhost:2040/api/uploadPre', {
       params: {
-        bucket: "my-bucket",
+        bucket: props.bucket,
         object_name: objectName
       }
     })
@@ -60,9 +37,9 @@ const uploadFile = async () => {
     const preSignedUrl = response.data.url
     
     // 使用预签名 URL 上传文件
-    const uploadResponse = await axios.put(preSignedUrl, file, {
+    const uploadResponse = await axios.put(preSignedUrl, selectedFile.value, {
       headers: {
-        'Content-Type': file.type
+        'Content-Type': selectedFile.value.type
       }
     })
     
@@ -78,19 +55,12 @@ const uploadFile = async () => {
 }
 
 const downloadFile = async () => {
-  if (!fileName.value) {
-    alert('Please enter a file name first.')
-    return
-  }
-  
-  const objectName = fileName.value
-  
   try {
     // 获取预签名 URL
     const response = await axios.get('http://localhost:2040/api/downloadPre', {
       params: {
-        bucket: "my-bucket",
-        object_name: objectName
+        bucket: props.bucket,
+        object_name: props.objectName
       }
     })
     
@@ -101,17 +71,50 @@ const downloadFile = async () => {
       responseType: 'blob'
     })
     
-    // 创建一个链接来下载文件
+    // 创建一个 Blob URL
     const url = window.URL.createObjectURL(new Blob([downloadResponse.data]))
     const link = document.createElement('a')
     link.href = url
-    link.setAttribute('download', objectName) // 文件名
+    link.download = props.objectName
     document.body.appendChild(link)
     link.click()
     document.body.removeChild(link)
+    window.URL.revokeObjectURL(url)
+    
+    alert('File downloaded successfully')
   } catch (error) {
     console.error('Error downloading file:', error)
     alert('Error downloading file')
   }
 }
+
+const transferFile = () => {
+  if (props.mode === 'upload') {
+    uploadFile()
+  } else {
+    downloadFile()
+  }
+}
+
+const replaceFileName = (fileName: string) => {
+  const extension = fileName.split('.').pop();
+  return `${props.objectName}.${extension}`;
+}
 </script>
+
+<template>
+  <div>
+    <input v-if="mode === 'upload'" type="file" @change="handleFileChange" />
+    <button @click="transferFile">{{ mode === 'upload' ? 'Upload' : 'Download' }}</button>
+  </div>
+</template>
+
+<style scoped>
+input {
+  margin-right: 10px;
+}
+
+button {
+  margin-right: 10px;
+}
+</style>
